461be05f28385f53235bb0f0bc14bc9416d4a492
kim8352 <tmak8dlf@naver.com>
2022-05-31 21:18:35 +0900

    about페이지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react";
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page???</title>
+      <title>Home Page</title>
       <h1>Home Page</h1>
     </main>
   );
