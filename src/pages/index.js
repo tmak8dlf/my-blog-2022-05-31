324310bc314472b69626c97b76340a122e1d1607
kim8352 <tmak8dlf@gmail.com>
2022-06-01 13:39:29 +0900

    블로그포스트페이지에서 입력된 슬러그를 바탕으로 올바른 포스트내용 불러와서 표시

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
-import Layout from "../components/layout";
+import layout from "../components/layout";
 
 const IndexPage = () => {
   return (
