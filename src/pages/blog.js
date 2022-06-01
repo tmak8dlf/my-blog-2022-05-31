b770de6c9b888ea6f26dd01f36dd21efe46e4cfc
kim8352 <tmak8dlf@gmail.com>
2022-06-01 13:11:47 +0900

    블로그 내용 표시

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,12 +2,32 @@ import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export default BlogPage;
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
+      }
+    }
+  }
+`;
