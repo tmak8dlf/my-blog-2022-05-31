1a9095115d0d3f79eab25ba8ae2a1c05e6e00efd
kim8352 <tmak8dlf@gmail.com>
2022-06-01 12:03:15 +0900

    graphQL로 data 가져와서 Layout에 표시

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "M_F Gatsby Site",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
