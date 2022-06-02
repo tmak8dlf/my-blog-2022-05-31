accfb071118c6a509d4e608fa42baa069450637a
kim8352 <tmak8dlf@gmail.com>
2022-06-02 13:25:02 +0900

    Gmarket font setting

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -5,30 +5,10 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 
 const BlogPost = ({ data }) => {
-  const image =
-    data.mdx.frontmatter.hero_image &&
-    getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
-      {image && (
-        <>
-          <GatsbyImage
-            image={image}
-            alt={data.mdx.frontmatter.hero_image_alt}
-          />
-          <p>
-            Photo Credit:{" "}
-            <a
-              target="_blank"
-              href={data.mdx.frontmatter.hero_image_credit_link}
-            >
-              {data.mdx.frontmatter.hero_image_credit_text}
-            </a>
-          </p>
-        </>
-      )}
     </Layout>
   );
 };
@@ -40,14 +20,6 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
-        hero_image_alt
-        hero_image_credit_link
-        hero_image_credit_text
-        hero_image {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
       }
     }
   }
