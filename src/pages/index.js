f23e776374d33f3dd56eabb8eef37eb9ac4b260f
kim8352 <tmak8dlf@gmail.com>
2022-06-02 10:21:45 +0900

    hero_image 필드가 파일 필드로 업로드

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { StaticImage } from "gatsby-plugin-image";
-import * as React from "react";
-import layout from "../components/layout";
+import React from "react";
+import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
